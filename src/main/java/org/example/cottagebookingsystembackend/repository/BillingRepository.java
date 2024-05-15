93a4867c5c8b979061c5a63764a7560fc680dfc7
leeviallu <68004427+leeviallu@users.noreply.github.com>
2024-05-15 14:59:32 +0300

    Query fixed

--- src/main/java/org/example/cottagebookingsystembackend/repository/BillingRepository.java
+++ src/main/java/org/example/cottagebookingsystembackend/repository/BillingRepository.java
@@ -9,7 +9,7 @@ import java.util.Date;
 import java.util.List;
 
 public interface BillingRepository extends CrudRepository<Billing, Long> {
-    @Query("SELECT r, s, b, sor FROM ServicesOfReservation sor JOIN Reservation r ON r.reservationId = sor.reservationId JOIN ServiceModel s ON s.serviceId = sor.serviceId JOIN Billing b ON b.reservationId = r.reservationId WHERE r.confirmationDate<:confirmationDate")
+    @Query("SELECT r, s, b, sor FROM Reservation r LEFT JOIN ServicesOfReservation sor ON r.reservationId = sor.reservationId LEFT JOIN ServiceModel s ON s.serviceId = sor.serviceId JOIN Billing b ON b.reservationId = r.reservationId WHERE r.confirmationDate<:confirmationDate")
     List<Object[]> findReservationByConfirmationDate(@Param("confirmationDate") Date confirmationDate);
 
     @Query("SELECT b FROM Billing b WHERE b.reservationId=:id")
